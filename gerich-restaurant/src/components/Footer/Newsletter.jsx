ef36b274b12c035128ad5e17f852f07fb1b9b75d
Fernando <fernandoalcocer812@hotmail.com>
2023-04-04 19:10:28 -0300

    Finalizada la pagina de Gericht

--- gerich-restaurant/src/components/Footer/Newsletter.jsx
+++ gerich-restaurant/src/components/Footer/Newsletter.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 
-import SubHeading from "../SubHeading/SubHeading";
+import { SubHeading } from "../../components";
 import "./Newsletter.css";
 
-const Newsletter = () => (
-  <div className="app__newsletter">
-    <div className="app__newsletter-heading">
-      <SubHeading title="Newsletter" />
+const Newsletter = () => {
+  return (
+    <div className="app__newsletter">
+      <div className="app__newsletter-heading">
+        <SubHeading title="Newsletter" />
+        <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
+        <p className="p__opensans">And Never Miss Latest Updates!</p>
+      </div>
+      <div className="app__newsletter-input flex__center">
+        <input type="email" placeholder="Enter your e-mail address" />
+        <button className="custom__button">Subscribe</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Newsletter;
